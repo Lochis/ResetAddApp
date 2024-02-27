6ca2b86bbcde53a0c8d5ef61d65132d8b3352c13
Lukowie <lukevdm64@gmail.com>
2024-02-26 23:30:40 -0500

    Parsed VASP spreadsheet columns into JSON format

--- routes/VASPspreadsheet.js
+++ routes/VASPspreadsheet.js
@@ -4,8 +4,12 @@ const express = require('express');
 const fs = require('fs');
 const zip = require('adm-zip');
 const { fileLoader } = require('ejs');
+const xlsx = require('node-xlsx');
+const {promisify} = require('util');
 const router = express.Router();
 
+const rename = promisify(fs.rename);
+const extractAllTo = promisify(new zip().extractAllTo);
 
 router.get('/', (req, res, next) => {
     try {
@@ -21,88 +25,101 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/get-spreadsheet', async (req, res) => {
-    // using puppeteer to mimic my log in onto ecno sharepoint location
-    async function loginMicrosoftPuppet (isAuth, name) {
-      if (isAuth) { 
-      const downloadPath = path.resolve('./routes/files/spreadsheet/');
-      const browser = await puppeteer.launch({headless: false});
-      const page = await browser.newPage();
-  
-      const client = await page.target().createCDPSession()
-      await client.send('Page.setDownloadBehavior', {
-        behavior: 'allow',
-        userDataDir: './',
-        downloadPath: downloadPath,
-      })
-  
-      const url = "https://ecno.sharepoint.com/sites/VASPDocumentPublication/Shared%20Documents/Forms/AllItems.aspx?FolderCTID=0x01200040DCD9A2FF9E1E42A3EF9EFD2DD260A1&id=%2Fsites%2FVASPDocumentPublication%2FShared%20Documents%2FGeneral%2FAPP%20STATUS%20SPREADSHEET&viewid=22be3505%2D5f01%2D42ae%2D9bae%2D5ba2d20af8c9";
-  
-      await page.setViewport({width: 1280, height: 800});
-      await page.goto(url);
-  
-      const navigationPromise = page.waitForNavigation({ waitUntil: 'networkidle0' });
-  
-      // type in email
-      await page.waitForSelector('#idSIButton9');
-      await page.type('#i0116', process.env.SPO_USERNAME);
-      // click next button
-      await page.click('#idSIButton9');
-      console.log("Got to email screen");
-  
-      await navigationPromise;
-  
-      // wait and type
-      const passwordBoxSelector = '#i0118';
-      await page.waitForSelector("#idSIButton9");
-      await page.type('#i0118', process.env.SPO_PASSWORD);
-      await page.click("#idSIButton9");
-      console.log("Got to Password screen");
-  
-      await navigationPromise;
-     
-      // click yes button
-      const textSelector = await page.waitForSelector('#idSIButton9');
-      await page.click("#idSIButton9");
-      console.log("Got to Stay Signed in screen");
-  
-      await navigationPromise;
-  
-      await page.waitForSelector("button[name='Download']");
-      await page.click("button[name='Download']");
-      await page.screenshot({ path: 'screenshot.png' });
+  // D:/Repos/RESETADDAPP/routes/files/spreadsheet\APP STATUS SPREADSHEET/VASP APP REQUEST STATUS Feb 22 2024.xlsx
+  //parseXLSX('D:/Repos/RESETADDAPP/routes/files/spreadsheet/APP STATUS SPREADSHEET/VASP APP REQUEST STATUS Feb 22 2024.xlsx')
+ 
+    await loginMicrosoftPuppet(req.session.isAuthenticated, req.session.account.name);
+    await processFiles();
 
-      setTimeout(() => {
-        let strName = name.split(" ");
-        strName = strName.join(".");
-        console.log(strName);
-    
-        
-      // gets the name of the latest file and renames it to VASP.zip
-      const latestFile = getLatestFile('./routes/files/spreadsheet/');
-  
-      fs.rename(latestFile, './routes/files/spreadsheet/VASP.zip', function (err) {
-          if (err) throw err
-          console.log('Successfully renamed - AKA moved!')
-      });
-          
-      // Now we can unzip the file and extract contents
-      var file = new zip('./routes/files/spreadsheet/VASP.zip');
-      file.extractAllTo(/*target path*/ downloadPath, /*overwrite*/ true);
-  
-      // Get a hold of spreadsheet location! (xlsx)
-      const spreadsheetFile = getLatestFile('./routes/files/spreadsheet/APP STATUS SPREADSHEET/');
-  
-      console.log(spreadsheetFile);
-      }, 2000);
-  
-    
-      }
-    };
-
-    loginMicrosoftPuppet(req.session.isAuthenticated, req.session.account.name);
-  
     res.json({message: "Spreadsheet gotten"});
   });
+
+
+
+// using puppeteer to mimic my log in onto ecno sharepoint location
+async function loginMicrosoftPuppet(isAuth, name) {
+  if (isAuth) {
+    const downloadPath = path.resolve('./routes/files/spreadsheet/');
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    const client = await page.target().createCDPSession()
+    await client.send('Page.setDownloadBehavior', {
+      behavior: 'allow',
+      userDataDir: './',
+      downloadPath: downloadPath,
+    })
+
+    const url = "https://ecno.sharepoint.com/sites/VASPDocumentPublication/Shared%20Documents/Forms/AllItems.aspx?FolderCTID=0x01200040DCD9A2FF9E1E42A3EF9EFD2DD260A1&id=%2Fsites%2FVASPDocumentPublication%2FShared%20Documents%2FGeneral%2FAPP%20STATUS%20SPREADSHEET&viewid=22be3505%2D5f01%2D42ae%2D9bae%2D5ba2d20af8c9";
+
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.goto(url);
+
+    const navigationPromise = page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+    // type in email
+    await page.waitForSelector('#idSIButton9');
+    await page.type('#i0116', process.env.SPO_USERNAME);
+    // click next button
+    await page.click('#idSIButton9');
+    console.log("Got to email screen");
+
+    await navigationPromise;
+
+    // wait and type
+    const passwordBoxSelector = '#i0118';
+    await page.waitForSelector("#idSIButton9");
+    await page.type('#i0118', process.env.SPO_PASSWORD);
+    await page.click("#idSIButton9");
+    console.log("Got to Password screen");
+
+    await navigationPromise;
+
+    // click yes button
+    const textSelector = await page.waitForSelector('#idSIButton9');
+    await page.click("#idSIButton9");
+    console.log("Got to Stay Signed in screen");
+
+    await navigationPromise;
+
+    await page.waitForSelector("button[name='Download']");
+    await page.click("button[name='Download']");
+    await page.screenshot({ path: 'screenshot.png' });
+
+    let strName = name.split(" ");
+    strName = strName.join(".");
+    console.log(strName);
+    
+  }
+};
+
+async function processFiles(){
+  setTimeout(() => {
+      // gets the name of the latest file and renames it to VASP.zip
+    const latestFile = getLatestFile('./routes/files/spreadsheet/');
+
+    fs.renameSync(latestFile, './routes/files/spreadsheet/VASP.zip');
+    console.log('Successfully renamed - AKA moved!') 
+
+    // Now we can unzip the file and extract contents
+    var file = new zip('./routes/files/spreadsheet/VASP.zip');
+    file.extractAllTo('./routes/files/spreadsheet/', /*overwrite*/ true);
+    console.log('Successfully unzipped file')
+
+     // Return the spreadsheet location! (xlsx)
+    const spreadsheetFile = getLatestFile('./routes/files/spreadsheet/APP STATUS SPREADSHEET/');
+     
+    console.log(spreadsheetFile);
+
+    parseXLSX(spreadsheetFile);
+  }, 4000);
+}
+
+  function parseXLSX(spreadsheet) {
+    const parsed = xlsx.parse(fs.readFileSync(spreadsheet));
+    console.log(parsed[0].data[13]);
+  }
+
 
 
   function getLatestFile(directoryPath) {
